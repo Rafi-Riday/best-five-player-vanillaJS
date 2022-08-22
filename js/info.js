e9402d81f6410532a64eb810e18cf902197008ef
Rafiul Hossain Riday <Rafi-Riday>
2022-08-22 14:19:27 +0600

    player information added

---
+++ js/info.js
@@ -0,0 +1,98 @@
+const players = [
+    {
+        name: "Lionel Messi",
+        position: "Forward",
+        club: "PSG",
+        achievements: [
+            ["THE BEST FIFA MEN'S PLAYER", 2], ["WINNER BALLON D'OR", 7], ["UEFA BEST PLAYER IN EUROPE", 3]
+        ],
+    },
+    {
+        name: "Cristiano Ronaldo",
+        position: "Forward",
+        club: "Man Utd",
+        achievements: [
+            ["THE BEST FIFA MEN'S PLAYER", 2], ["WINNER BALLON D'OR", 5], ["UEFA BEST PLAYER IN EUROPE", 4]
+        ],
+    },
+    {
+        name: "Sergio Ramos",
+        position: "Defender",
+        club: "PSG",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 4], ["EUROPEAN CHAMPION", 2]
+        ],
+    },
+    {
+        name: "Xavi",
+        position: "Midfielder",
+        club: "Al-Sadd SC",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 2], ["EUROPEAN CHAMPION", 2]
+        ],
+    },
+    {
+        name: "Andres Iniesta",
+        position: "Midfielder",
+        club: "Vissel Kobe",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 3], ["EUROPEAN CHAMPION", 2]
+        ],
+    },
+    {
+        name: "Gianluigi Buffon",
+        position: "Goalkeeper",
+        club: "Parma",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 10], ["FRENCH CHAMPION", 1]
+        ],
+    },
+    {
+        name: "Iker Casillas",
+        position: "Goalkeeper",
+        club: "FC Porto",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 1], ["EUROPEAN CHAMPION", 2]
+        ],
+    },
+    {
+        name: "Zlatan Ibrahimovic",
+        position: "Forward",
+        club: "AC Milan",
+        achievements: [
+            ["FIFA CLUB WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 5], ["FRENCH CHAMPION", 4]
+        ],
+    },
+    {
+        name: "Radamel Falcao",
+        position: "Forward",
+        club: "Rayo Vallecano",
+        achievements: [
+            ["FRENCH CHAMPION", 1], ["EUROPA LEAGUE WINNER", 2], ["UEFA SUPERCUP WINNER", 1]
+        ],
+    },
+    {
+        name: "Robin van Persie",
+        position: "Forward",
+        club: "Feyenoord Rotterdam",
+        achievements: [
+            ["ENGLISH CHAMPION", 1], ["UEFA CUP WINNER", 1], ["ENGLISH FA CUP WINNER", 1]
+        ],
+    },
+    {
+        name: "Thiago Silva",
+        position: "Defender",
+        club: "Chelsea",
+        achievements: [
+            ["FIFA CLUB WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 1], ["FRENCH CHAMPION", 7]
+        ],
+    },
+    {
+        name: "Sergio Busquets",
+        position: "Defender",
+        club: "Barcelona",
+        achievements: [
+            ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 3], ["EUROPEAN CHAMPION", 1]
+        ],
+    },
+]
