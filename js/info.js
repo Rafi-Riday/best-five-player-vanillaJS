4ca9657a6cf9541610a138b56d6aa8666adb6fdb
Rafiul Hossain Riday <Rafi-Riday>
2022-08-22 16:07:22 +0600

    more information added

--- js/info.js
+++ js/info.js
@@ -3,6 +3,7 @@ const players = [
         name: "Lionel Messi",
         position: "Forward",
         club: "PSG",
+        career: [{ goals: 769 }, { assists: 331 }],
         achievements: [
             ["THE BEST FIFA MEN'S PLAYER", 2], ["WINNER BALLON D'OR", 7], ["UEFA BEST PLAYER IN EUROPE", 3]
         ],
@@ -11,6 +12,7 @@ const players = [
         name: "Cristiano Ronaldo",
         position: "Forward",
         club: "Man Utd",
+        career: [{ goals: 815 }, { assists: 273 }],
         achievements: [
             ["THE BEST FIFA MEN'S PLAYER", 2], ["WINNER BALLON D'OR", 5], ["UEFA BEST PLAYER IN EUROPE", 4]
         ],
@@ -19,6 +21,7 @@ const players = [
         name: "Sergio Ramos",
         position: "Defender",
         club: "PSG",
+        career: [{ goals: 107 }, { assists: 39 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 4], ["EUROPEAN CHAMPION", 2]
         ],
@@ -27,6 +30,7 @@ const players = [
         name: "Xavi",
         position: "Midfielder",
         club: "Al-Sadd SC",
+        career: [{ goals: 108 }, { assists: 213 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 2], ["EUROPEAN CHAMPION", 2]
         ],
@@ -35,6 +39,7 @@ const players = [
         name: "Andres Iniesta",
         position: "Midfielder",
         club: "Vissel Kobe",
+        career: [{ goals: 88 }, { assists: 163 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 3], ["EUROPEAN CHAMPION", 2]
         ],
@@ -43,6 +48,7 @@ const players = [
         name: "Gianluigi Buffon",
         position: "Goalkeeper",
         club: "Parma",
+        career: [{ clean_sheets: 424 }, { goals_conceded: 801 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 10], ["FRENCH CHAMPION", 1]
         ],
@@ -51,6 +57,7 @@ const players = [
         name: "Iker Casillas",
         position: "Goalkeeper",
         club: "FC Porto",
+        career: [{ clean_sheets: 338 }, { goals_conceded: 867 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 1], ["EUROPEAN CHAMPION", 2]
         ],
@@ -59,6 +66,7 @@ const players = [
         name: "Zlatan Ibrahimovic",
         position: "Forward",
         club: "AC Milan",
+        career: [{ goals: 492 }, { assists: 204 }],
         achievements: [
             ["FIFA CLUB WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 5], ["FRENCH CHAMPION", 4]
         ],
@@ -67,6 +75,7 @@ const players = [
         name: "Radamel Falcao",
         position: "Forward",
         club: "Rayo Vallecano",
+        career: [{ goals: 222 }, { assists: 29 }],
         achievements: [
             ["FRENCH CHAMPION", 1], ["EUROPA LEAGUE WINNER", 2], ["UEFA SUPERCUP WINNER", 1]
         ],
@@ -75,6 +84,7 @@ const players = [
         name: "Robin van Persie",
         position: "Forward",
         club: "Feyenoord Rotterdam",
+        career: [{ goals: 274 }, { assists: 102 }],
         achievements: [
             ["ENGLISH CHAMPION", 1], ["UEFA CUP WINNER", 1], ["ENGLISH FA CUP WINNER", 1]
         ],
@@ -83,6 +93,7 @@ const players = [
         name: "Thiago Silva",
         position: "Defender",
         club: "Chelsea",
+        career: [{ goals: 37 }, { assists: 17 }],
         achievements: [
             ["FIFA CLUB WORLD CUP WINNER", 1], ["ITALIAN CHAMPION", 1], ["FRENCH CHAMPION", 7]
         ],
@@ -91,6 +102,7 @@ const players = [
         name: "Sergio Busquets",
         position: "Defender",
         club: "Barcelona",
+        career: [{ goals: 18 }, { assists: 43 }],
         achievements: [
             ["WORLD CUP WINNER", 1], ["FIFA CLUB WORLD CUP WINNER", 3], ["EUROPEAN CHAMPION", 1]
         ],
